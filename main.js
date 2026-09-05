319936125b2cf22636ad04061b9fc59a0f7bdc8e
Mohamed Khaled <mohamedkhaled65556@gmail.com>
2026-09-05 12:58:11 +0300

    Implement password change and balance visibility toggle
    Added functionality to change password and show/hide balance.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let balance = 0;
 let passward = "1234";
 let historyTrans = [];
 let isOnePassword = false;
+let isShowBalance = false;
 let span = document.querySelector("#Balance");
 
 const oneTimePass = () => {
@@ -15,11 +16,36 @@ const oneTimePass = () => {
     }
   }
 };
-const showBalance = () => {
+
+const changePassword = () => {
   oneTimePass();
   if (isOnePassword) {
-    span.innerHTML = `
+    let input = document.querySelector("#Password");
+    let newPassword = input.value;
+    input.value = "";
+    if (newPassword === "") alert("كلمة المرور الجديده لا يمكن ان تكون فارغه");
+    else if (newPassword.length < 4)
+      alert("كلمة المرور الجديده لازم ع الاقل تكون مكونه من 4 أحرف");
+    else if (newPassword === passward) alert("كلمة المرور هذه مستخدمه من قبل");
+    else {
+      passward = newPassword;
+      alert("تم تغيير كلمة المرور");
+      isOnePassword = false;
+    }
+  }
+};
+
+const showAndHideBalance = () => {
+  oneTimePass();
+  if (isOnePassword) {
+    if (!isShowBalance) {
+      span.innerHTML = `
     <span id="Balance" type="number">${balance} جنيه</span>`;
+      isShowBalance = true;
+    } else {
+      span.innerHTML = `<span id="Balance">**** جنيه</span>`;
+      isShowBalance = false;
+    }
   }
 };
 
